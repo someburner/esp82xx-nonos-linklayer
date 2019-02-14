5a4f10eefa665b0da3bf256f9e093702ced1ba7d
Jeff H <jeffrey92@gmail.com>
2019-02-13 22:27:12 -0800

    add numbered target define for per-target ifdefs in src

--- glue-lwip/lwip-git.c
+++ glue-lwip/lwip-git.c
@@ -386,7 +386,11 @@ void esp2glue_lwip_init (void)
 	sntp_servermode_dhcp(1); /* get SNTP server via DHCP */
 	sntp_setoperatingmode(SNTP_OPMODE_POLL);
 	// start anyway the offline sntp timer
+#if TARGET==1
 	SNTP_SET_SYSTEM_TIME_US(0,0);
+#else
+	sntp_set_system_time(0);
+#endif
 }
 
 void esp2glue_alloc_for_recv (size_t len, void** pbuf, void** data)
